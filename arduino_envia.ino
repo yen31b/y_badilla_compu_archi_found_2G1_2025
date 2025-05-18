96d7d064e4df85d26e69cc0cf3e02ebb6d7927f5
MeyMora <112515882+MeyMora@users.noreply.github.com>
2025-05-17 22:09:26 -0600

    Agregue el pin CS para iniciar y terminar la comunicación entre FPGA-Arduino, este seria Chip-Select

--- arduino_envia.ino
+++ arduino_envia.ino
@@ -1,10 +1,17 @@
+const int csPin = 8;  // Pin de Chip Select (CS)
+
 void setup() {
-  Serial.begin(9600);  // velocidad UART
+  Serial.begin(9600);       // Velocidad UART
+  pinMode(csPin, OUTPUT);   // Configurar el pin CS como salida
+  digitalWrite(csPin, HIGH); // Inicialmente deshabilitado (HIGH)
 }
 
 void loop() {
   for (int i = 0; i < 16; i++) {
-    Serial.write(i);   // enviar número binario de 0 a 15
-    delay(1000);       // espera de 1 segundo
+    digitalWrite(csPin, LOW);   // Habilitar comunicación (CS activo bajo)
+    Serial.write(i);            // Enviar número binario de 0 a 15
+    delay(500);                 // Mantener CS activo un tiempo visible
+    digitalWrite(csPin, HIGH);  // Deshabilitar comunicación (CS desactivado)
+    delay(1000);                // Espera de 1 segundo
   }
 }
